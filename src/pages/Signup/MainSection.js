a265b7174b553b6b75e35d3f1855e4d4010db7da
Jakub Hojsan <jakubhojsan@g.ucla.edu>
2021-11-04 02:10:55 -0700

    Small changes related to UI, colors, and signup page
    I needed to better follow the team's Figma guidelines and replace the bruin colors with the offical color way.
    I also needed to make the signup cards more realistic.

--- src/pages/Signup/MainSection.js
+++ src/pages/Signup/MainSection.js
@@ -3,6 +3,9 @@ import useStyles from './signup-styles'
 import { Typography, Button, Container, Grid, CardMedia, CardContent, CardActions, Card, Stack} from '@mui/material';
 import { Link } from 'react-router-dom';
 import BasicCard from '../BasicCard';
+import image1 from '../../images/dining.jpeg'
+import image2 from '../../images/background.jpeg'
+
 
 
 function MainSection() {
@@ -14,8 +17,48 @@ function MainSection() {
         <div className={classes.contained} style={{backgroundColor: "#f7f7f7"}}> 
             <Container maxWidth="md">
                 <Stack className={classes.stackmiddle} direction="row" justifyContent="space-between">
-                    <BasicCard/>
-                    <BasicCard/>
+                        <Card className={classes.card}>
+                            <CardMedia className={classes.cardMedia} image={image2} title="Image Title"></CardMedia>
+                            <CardContent className={classes.cardContent}>
+                                <Typography
+                                
+                                styles={{
+                                    display: 'flex', 
+                                    alignItems: 'center', 
+                                    justifyContent:'center'
+                               }}
+
+                                
+                                className={classes.middletext} gutterBottom variant="h5">
+                                    I am a student
+                                </Typography>
+                                <Typography  color="textSecondary">
+                                    Join any queue on campus in real-time!
+                                </Typography>
+                            </CardContent>
+                            <CardActions style={{padding: "15px"}}>
+                                <Link style={{textDecoration: 'none'}} to={{pathname: "/card", state: {card: {text: "This is a card!", image: "https://source.unsplash.com/random"}, hasValue: true}}}>
+                                    <Button size="small" variant="contained" color="primary" className={classes.cardButtons}>Login with Google</Button>
+                                </Link>
+                            </CardActions>
+                        </Card>
+
+                        <Card className={classes.card}>
+                            <CardMedia className={classes.cardMedia} image={image1} title="Image Title"></CardMedia>
+                            <CardContent className={classes.cardContent}>
+                                <Typography className={classes.middletext} gutterBottom variant="h5">
+                                    I am a resturant
+                                </Typography>
+                                <Typography color="textSecondary">
+                                    Manage your profile and queue time!
+                                </Typography>
+                            </CardContent>
+                            <CardActions style={{padding: "15px"}}>
+                                <Link style={{textDecoration: 'none'}} to={{pathname: "/card", state: {card: {text: "This is a card!", image: "https://source.unsplash.com/random"}, hasValue: true}}}>
+                                    <Button size="small" variant="contained" color="primary" className={classes.cardButtons}>Login with Google</Button>
+                                </Link>
+                            </CardActions>
+                        </Card>
                 </Stack>
             </Container>
         </div>
