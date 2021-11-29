928d7e9ae9753bf01cb817be159a34c34c93678f
Hritik <hritikarya1402@gmail.com>
2021-11-28 21:46:12 -0800

    Raised each card into a Person.js file. Passed names and description as props.

--- src/pages/AboutUs/MainSection.js
+++ src/pages/AboutUs/MainSection.js
@@ -7,45 +7,44 @@ import { Link } from 'react-router-dom';
 function MainSection() {
     const classes = useStyles();
     return (
-        <div>
+        <div className={classes.contained} style={{backgroundColor: "#f7f7f7"}}>
             <Container maxWidth="md">
                 <Grid container spacing={2} alignItems="stretch">
-                    <Card className={classes.card}>
-                        <CardMedia 
-                            component="img"
-                            image="https://source.unsplash.com/random"
-                            height="140"
-                        />
-                        <CardContent className={classes.cardContent}>
-                            <Typography gutterBottom align="center" variant="h5" component="div">
-                                Jake Sager
-                            </Typography>
-                            <Typography variant="body2" color="text.secondary">
-                                Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime mollitia,
-                                molestiae quas vel sint commodi repudiandae consequuntur voluptatum laborum
-                                numquam blanditiis harum quisquam eius sed odit fugiat iusto fuga praesentium
-                                optio, eaque rerum! Provident
-                            </Typography>
-                        </CardContent>
-                    </Card>
-                    <Card className={classes.card}>
-                        <CardMedia 
-                            component="img"
-                            image="https://source.unsplash.com/random"
-                            height="140"
-                        />
-                        <CardContent className={classes.cardContent}>
-                            <Typography gutterBottom align="center" variant="h5" component="div">
-                                Jake Sager
-                            </Typography>
-                            <Typography variant="body2" color="text.secondary">
-                                Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime mollitia,
-                                molestiae quas vel sint commodi repudiandae consequuntur voluptatum laborum
-                                numquam blanditiis harum quisquam eius sed odit fugiat iusto fuga praesentium
-                                optio, eaque rerum! Provident
-                            </Typography>
-                        </CardContent>
-                    </Card>
+                    <Person 
+                        name="Jake Sager"
+                        description="Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime mollitia,
+                        molestiae quas vel sint commodi repudiandae consequuntur voluptatum laborum
+                        numquam blanditiis harum quisquam eius sed odit fugiat iusto fuga praesentium
+                        optio, eaque rerum!"
+                    />
+                    <Person 
+                        name="Avii Ahuja"
+                        description="Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime mollitia,
+                        molestiae quas vel sint commodi repudiandae consequuntur voluptatum laborum
+                        numquam blanditiis harum quisquam eius sed odit fugiat iusto fuga praesentium
+                        optio, eaque rerum!"
+                    />
+                    <Person 
+                        name="Hritik Arya"
+                        description="Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime mollitia,
+                        molestiae quas vel sint commodi repudiandae consequuntur voluptatum laborum
+                        numquam blanditiis harum quisquam eius sed odit fugiat iusto fuga praesentium
+                        optio, eaque rerum!"
+                    />
+                    <Person 
+                        name="Molly Wehrenberg"
+                        description="Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime mollitia,
+                        molestiae quas vel sint commodi repudiandae consequuntur voluptatum laborum
+                        numquam blanditiis harum quisquam eius sed odit fugiat iusto fuga praesentium
+                        optio, eaque rerum!"
+                    />
+                    <Person 
+                        name="Jakub Hojsan"
+                        description="Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime mollitia,
+                        molestiae quas vel sint commodi repudiandae consequuntur voluptatum laborum
+                        numquam blanditiis harum quisquam eius sed odit fugiat iusto fuga praesentium
+                        optio, eaque rerum!"
+                    />
                 </Grid>
             </Container>
         </div>
--- src/pages/AboutUs/Person.js
+++ src/pages/AboutUs/Person.js
@@ -1,17 +1,35 @@
 import React from 'react'
 import useStyles from './about-styles'
+import { Typography, AppBar, Button, Card, CardActions, CardContent, CardMedia, CssBaseline, Grid, Toolbar, Container, CardActionArea } from '@mui/material'
 
 function Person (props) {
     const classes = useStyles()
     return(
-        <div className={classes.personcontainer}>
-            <div className={classes.imagecontainer}>
-                <img className={classes.displaypic} src="https://source.unsplash.com/random" />
-            </div>
-            <div className={classes.namecontainer}>
-                {props.name}
-            </div>
-        </div>
+        // <div className={classes.personcontainer}>
+        //     <div className={classes.imagecontainer}>
+        //         <img className={classes.displaypic} src="https://source.unsplash.com/random" />
+        //     </div>
+        //     <div className={classes.namecontainer}>
+        //         {props.name}
+        //     </div>
+        // </div>
+        <Grid item xs={12} sm={6} md={4}>
+            <Card className={classes.card}>
+                <CardMedia 
+                    component="img"
+                    image="https://source.unsplash.com/random"
+                    height="140"
+                />
+                <CardContent className={classes.cardContent}>
+                    <Typography gutterBottom align="center" variant="h5" component="div">
+                        {props.name}
+                    </Typography>
+                    <Typography variant="body2" color="text.secondary">
+                        {props.description}
+                    </Typography>
+                </CardContent>
+            </Card>
+        </Grid>
     )
 }
 
